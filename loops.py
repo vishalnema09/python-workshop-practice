d4e08d0bf3a14dd11e90f9ec179ea2322a49207c
Vishal Nema <vishalnema23@gmail.com>
2025-08-01 19:03:17 +0530

    Basic operation in List

---
+++ loops.py
@@ -0,0 +1,30 @@
+list_of_cloud = ["AWS", "Azure", "GCP" , "IBM Cloud", "Oracle Cloud"]
+print(list_of_cloud)
+
+#add a new cloud provider
+list_of_cloud.append("DigitalOcean")
+print(list_of_cloud)
+
+#add a new cloud provider at the beginning
+list_of_cloud.insert(0, "Alibaba Cloud")
+print(list_of_cloud)
+
+#add a new cloud provider at index 2
+list_of_cloud.insert(2, "Linode")
+print(list_of_cloud)
+
+print(len(list_of_cloud))  # Print the length of the list
+
+
+
+# Remove a cloud provider
+list_of_cloud.remove("IBM Cloud")
+print(list_of_cloud)
+
+# Remove the last cloud provider
+list_of_cloud.pop()
+print(list_of_cloud)
+
+print("The first cloud provider is:", list_of_cloud[0])
+print("The cloud provider at index 2 is:", list_of_cloud[2])
+print("The last cloud provider is:", list_of_cloud[-1])
